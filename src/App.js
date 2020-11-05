c52dc302ea3891c950f22934107dfeab83793af5
Eliram <eliram@MacBook-Pro.local>
2020-11-04 19:19:06 -0500

    Search component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 
 import CryptoPrices from './components/CryptoPrices';
 import Footer from './components/Footer';
+import Search from './components/Search';
 
 class App extends Component {
     constructor(props) {
@@ -17,6 +18,7 @@ class App extends Component {
             <div className="App text-light">
                 <h1 className="display-3 pt-4">Crypto Tracker</h1>
                 <CryptoPrices />
+                <Search />
                 <Footer />
             </div>
         );
---
+++ src/components/Search.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Search = () => {
+    return ( 
+        <div className="search container">
+            <div className="bg-warning">
+                Search!
+            </div>            
+        </div>
+    );
+}
+ 
+export default Search;
