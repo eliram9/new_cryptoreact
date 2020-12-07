9cb183524db631bf73a23cb625339f6f5cdb9554
Eliram <eliram@MacBook-Pro.local>
2020-12-06 21:52:53 -0500

    added "not found" msg for search function

--- src/components/SearchResult.jsx
+++ src/components/SearchResult.jsx
@@ -7,38 +7,51 @@ import './SearchResult.css';
 const SearchResult = () => {
     const [coin, setCoin] = useState('');
     const [isLoading, setIsLoading] = useState(true);
+    const [firstLanding, setFirstLanding] = useState(true);
+    const [notFound, setNotFound] = useState('');
     const [searchHistory, setSearchHistory] = useState([]); // --> my next step for serach results box
+    
     useEffect(() => {
         searchCoin();
     }, [])
 
     const searchCoin = async (searchInput) => {
-        // const coinSearchResult = [];
-        const URL = `https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&ids=${searchInput}&order=market_cap_desc&per_page=2000&page=1&sparkline=false`;
+        // const coinResult = [];
         // const API_KEY = '5f1669f9639c90e3b179bbd4e9569d83195316fd3252fdf9a54b0604120f1d1e'
         // const URL = `https://min-api.cryptocompare.com/data/pricemultifull?fsyms=${searchInput}&tsyms=USD&api_key={${API_KEY}}`;
+        setNotFound('');
+        setCoin('');
+        setIsLoading(true)
+        const URL = `https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&ids=${searchInput}&order=market_cap_desc&per_page=2000&page=1&sparkline=false`;
         const getSearchedCoin = await axios.get(URL);
         let results = await getSearchedCoin.data;
         console.log(results);
-;
-        // coinSearchResult.push(results);
-        // console.log(coinSearchResult);
+            if (results === undefined || results.length == 0 && firstLanding === true) {
+                setFirstLanding(false);
+                console.log('initial');
+            }
 
-            for (let index in results) {
+            else if (results == 0) {
+                setNotFound('Coin was not found 🙁');
+            }
+
+            else {
+                for(let index in results) {
                     let coin = results[index];
-                    searchHistory.push(coin);
-                    //setting the last coin here
+                    // searchHistory.push(coin);
                     setCoin(coin);
                     setIsLoading(false);
                     // console.log(coin)
+                }
+                // console.log(searchHistory);
             }
-            // console.log(searchHistory);
     }
     
-
     return ( 
         <div className="searchResult">
             <SearchBar searchCoin={searchCoin} />
+            {/* In case the coin was not found */}
+            <h4>{notFound}</h4>
                 <div className="container">
                 {isLoading ? (
                     <div>
