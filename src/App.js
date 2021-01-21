1bc8d08d53ec7dc82881d3fadf52535d45bccc91
eliram9 <eliram9@gmail.com>
2021-01-20 23:32:35 -0500

    css responsive

--- src/App.js
+++ src/App.js
@@ -1,30 +1,22 @@
-import React, { Component } from 'react';
+import React from 'react';
 
 import './App.css';
 
 import CryptoPrices from './components/CryptoPrices';
 import Footer from './components/Footer';
-import SearchBar from './components/SearchBar';
+//import SearchBar from './components/SearchBar';
 import SearchResult from './components/SearchResult';
 
 
-class App extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {} 
-    
-    }
-
-    render() { 
-        return ( 
-            <div className="App text-light">
-                <h1 className="display-3 pt-4">Crypto Tracker</h1>
-                <CryptoPrices />
-                <SearchResult />
-                <Footer />
-            </div>
-        );
-    }
+const App = () => {
+    return (  
+        <div className="App text-light">
+            <h1 className="display-3 pt-2">Crypto Tracker</h1>
+            <CryptoPrices />
+            <SearchResult />
+            <Footer />
+        </div>
+    );
 }
  
 export default App;
